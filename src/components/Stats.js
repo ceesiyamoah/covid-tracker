e50531ffff99fb7a56fbd7a01980b667065bd178
ceesiyamoah <ccyamoah@gmail.com>
2020-11-25 00:16:31 +0000

    Implemented data flow between components

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,17 +1,13 @@
 import React from 'react';
-const Stats = () => {
-	const num = 12442;
-	return (
-		<div className='stats-container'>
-			<div>
-				<h4>Aggregated confirmed</h4>
-				<span style={{ color: '#ab4432' }}>{num.toLocaleString('en')}</span>
-			</div>
-			<div>2nd</div>
-			<div>3rd</div>
-			<div>4th</div>
+const Stats = ({ details }) => {
+	const detailsRender = details.map(({ name, value, color }) => (
+		<div key={name}>
+			<h4>{name}</h4>
+			<span style={{ color }}>{value.toLocaleString('en')}</span>
 		</div>
-	);
+	));
+
+	return <div className='stats-container'>{detailsRender}</div>;
 };
 
 export default Stats;
